041c0787fa067145a6b34be86976017924270f4d
Hakan Sandell <hakan.sandell@home.se>
2009-05-25 21:37:42 +0200

    Swedish translation

---
+++ lang/sv/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * swedish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Håkan Sandell <hakan.sandell[at]mydata.se>
+ */
+
+$lang['pageheader']            = 'Administration';
+$lang['wiki_version']          = 'DokuWiki version:';
+$lang['php_version']           = 'PHP version:';
+$lang['more_adminheader']      = 'Fler Administrativa Insticksmoduler';
+$lang['more_admintext']        = 'Nedan hittar du en lista över administrativa uppgifter i denna DokuWiki. Flera kan läggas till via [[doku>plugins?plugintype=2|admin insticksmoduler]].';
